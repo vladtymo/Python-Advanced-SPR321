bcaa09e9fb5607709b374c4c2820afd595aa3a2a
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-12-14 09:32:47 +0200

    Generators

---
+++ advanced/generators.py
@@ -0,0 +1,54 @@
+# -------------- generators
+# yield vs return
+def testYield():
+    print("---1---")
+    yield 1
+    print("---2---")
+    yield 2
+    print("---3---")
+    yield 3
+
+
+# print(testYield())
+
+for i in testYield():
+    print(i)
+
+
+# - 1,1,2,3,5,8,13...
+def fibonacci_numbers(max):
+    x, y = 1, 1
+    while x <= max:
+        yield x
+
+        # if x % 7 == 0:
+        #     break
+
+        temp = y
+        y = x + y
+        x = temp
+
+
+# for x in fibonacci_numbers(100):
+#     input()  # pause
+#     print(x)
+
+# -------- Generator Expression Syntax
+# print("-------------- Generator Expression")
+
+
+def powOfTwo():
+    for i in range(10):
+        yield pow(2, i)
+
+
+# syntax: [expresion, for var in collection (condition)]
+result = [pow(2, i) for i in range(10)]
+
+for i in powOfTwo():
+    print(i, end=" ")
+
+print("\n----- using gen exp")
+
+for i in result:
+    print(i, end=" ")
