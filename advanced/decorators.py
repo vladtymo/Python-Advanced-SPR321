99f4ffe56040504a8b138e6892107416c6645bbe
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-05-31 14:31:52 +0300

    Update

--- advanced/decorators.py
+++ advanced/decorators.py
@@ -1,19 +1,22 @@
-def testDecorator(input_func):
+def withBorders(input_func):
     def output_func(*args):
+        count = 10
+        if isinstance(args[0], str):
+            count = len(args[0]) - 2
+        print(f"┏{"*" * count}┓")
         # extra logic
-        count = len(args[0])
-        print("-" * count)
         input_func(*args)
-        print("-" * count)
         # extra logic
+        print(f"┗{"*" * count}┛")
 
     return output_func
 
 
-@testDecorator
+@withBorders
 def showMessage(text):
     print(text)
 
-
 showMessage("Hello")
 showMessage("Have a good day!")
+showMessage("How are you doing today?")
+showMessage("See you later!")
